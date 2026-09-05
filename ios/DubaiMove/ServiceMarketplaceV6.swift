36a58728b81287fc5ddf42168e0030fb802c24ef
cakirizm <cakir.dubai@gmail.com>
2026-09-06 03:21:50 +0400

    Route My Move to complete operations suite

--- ios/DubaiMove/ServiceMarketplaceV6.swift
+++ ios/DubaiMove/ServiceMarketplaceV6.swift
@@ -9,7 +9,7 @@ struct PremiumRootTabViewV6: View {
                 .tag(MainTab.home)
                 .tabItem { Label("Home", systemImage: "house.fill") }
 
-            NavigationStack { SmartMoveCommandCenterView() }
+            NavigationStack { CompleteMoveCommandCenterView() }
                 .tag(MainTab.move)
                 .tabItem { Label("My Move", systemImage: "list.number") }
 
